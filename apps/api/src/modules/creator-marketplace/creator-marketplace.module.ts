8a484964627c35212f11a2cb96c65dd93c8964a1
blue45f <blue45f@gmail.com>
2026-09-04 10:54:01 +0900

    feat(market): register social API runtime

--- apps/api/src/modules/creator-marketplace/creator-marketplace.module.ts
+++ apps/api/src/modules/creator-marketplace/creator-marketplace.module.ts
@@ -4,6 +4,8 @@ import { CreatorMarketplaceLibraryController } from "./creator-marketplace-libra
 import { CREATOR_MARKETPLACE_LIBRARY_REPOSITORY_PROVIDER } from "./creator-marketplace-library.repository";
 import { CreatorMarketplaceLibraryService } from "./creator-marketplace-library.service";
 import { creatorMarketplacePublishGateProvider } from "./creator-marketplace-publish-gate.repository";
+import { CreatorMarketplaceSocialController } from "./creator-marketplace-social.controller";
+import { CreatorMarketplaceSocialService } from "./creator-marketplace-social.service";
 import { CreatorMarketplaceController } from "./creator-marketplace.controller";
 import { creatorMarketplaceResourceRepositoryProvider } from "./creator-marketplace.repository";
 import { CreatorMarketplaceService } from "./creator-marketplace.service";
@@ -12,6 +14,7 @@ import { CreatorMarketplaceService } from "./creator-marketplace.service";
   controllers: [
     CreatorMarketplaceController,
     CreatorMarketplaceLibraryController,
+    CreatorMarketplaceSocialController,
   ],
   providers: [
     CREATOR_MARKETPLACE_LIBRARY_REPOSITORY_PROVIDER,
@@ -19,6 +22,7 @@ import { CreatorMarketplaceService } from "./creator-marketplace.service";
     creatorMarketplacePublishGateProvider,
     creatorMarketplaceResourceRepositoryProvider,
     CreatorMarketplaceService,
+    CreatorMarketplaceSocialService,
   ],
 })
 export class CreatorMarketplaceModule {}
