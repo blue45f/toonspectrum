176b5f68e5c77d462222b7b6d6f84407ea2dff4b
blue45f <blue45f@gmail.com>
2026-09-19 07:18:32 +0900

    refactor(routes): keep creator registry within size ratchet

--- apps/web/src/app/routes/groups/creator.routes.tsx
+++ apps/web/src/app/routes/groups/creator.routes.tsx
@@ -1,9 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 import { StudioHomeEntryRoute } from "@/domains/creator/studio-router/StudioHomeEntryRoute";
-
 import { studioRoutePath } from "@/domains/creator/studio-route-registry";
-
 import { defineAppRoutes } from "../app-route-definition";
 import {
   CharacterShaperLandingPage,
